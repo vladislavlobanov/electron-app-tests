eb71b4896d164424302f82b465093452c7be7133
Vaisakh Sasikumar <vaisakh.sasikumar@3tsoftwarelabs.com>
2024-12-18 17:32:05 +0100

    Run query and check result

--- test/specs/acceptance-tests/test.e2e.ts
+++ test/specs/acceptance-tests/test.e2e.ts
@@ -1,9 +1,20 @@
 import { browser } from '@wdio/globals'
 import { assert } from 'chai';
 
-describe('Electron Testing', () => {
-    it('should test application title', async () => {
-        assert.strictEqual(await browser.getTitle(), 'MongoDB Query Executor', 'Incorrect application title');
-    })
-})
 
+describe('MongoDB Query Execution Test', () => {
+    it('should execute a simple query and display results', async () => {
+        const queryInput = await browser.$('.card-body textarea');
+        await queryInput.setValue('{}');
+
+        const runButton = await browser.$('.btn.btn-primary.btn-block');
+        await runButton.click();
+
+        const resultElement = await browser.$('.card-body pre');
+        const resultText = await resultElement.getText();
+
+        assert.notInclude(resultText, 'Invalid Query', 'Query result should not contain "Invalid Query"');
+        assert.include(resultText, '{', 'Query result should start with {"');
+
+    });
+});
