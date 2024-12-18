8b2b8ea4d98e11ab4194badd6f65451c669c928c
andrei-kim-3tsoftwarelabs <andrei.kim@3tsoftwarelabs.com>
2024-12-18 17:01:54 +0100

    update install script

--- automated_testing/install.sh
+++ automated_testing/install.sh
@@ -3,8 +3,10 @@
 # Variables
 APP_NAME="MongoDB Query Executor"
 DMG_FILE="MongoDB-Query-Executor-Mac-1.0.0-Installer.dmg"
-MOUNT_DIR="/Volumes/$APP_NAME"
+MOUNT_NAME="MongoDB Query Executor 1.0.0-arm64"
+MOUNT_DIR="/Volumes/$MOUNT_NAME"
 DMG_URL="https://github.com/vaisakhsasikumar/my-electron-app/releases/download/v1.0.0/MongoDB-Query-Executor-Mac-1.0.0-Installer.dmg"
+
 # Step 1: Download the DMG file
 echo "Downloading DMG..."
 curl -L $DMG_URL -o $DMG_FILE
@@ -23,6 +25,8 @@ hdiutil detach "$MOUNT_DIR" -quiet
 
 # Step 5: Verify the app exists
 if [ -d "/Applications/$APP_NAME.app" ]; then
+  # Fix warnings from Non App Store applications
+  xattr -c "/Applications/$APP_NAME.app"
   echo "$APP_NAME installed successfully!"
 else
   echo "Installation failed!"
