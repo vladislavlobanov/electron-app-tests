6291638f6bdcdbe27fd3e00bf7eb434bb51357a9
Vaisakh Sasikumar <vaisakhsasikumar@users.noreply.github.com>
2024-12-19 16:23:25 +0100

    Update test.e2e.ts

--- test/specs/acceptance-tests/test.e2e.ts
+++ test/specs/acceptance-tests/test.e2e.ts
@@ -14,7 +14,12 @@ describe('MongoDB Query Execution Test', () => {
         const resultText = await resultElement.getText();
 
         assert.notInclude(resultText, 'Invalid Query', 'Query result should not contain "Invalid Query"');
-        assert.include(resultText, '[]', 'Query result should start with []"');
+        const expectedText = JSON.stringify([
+          { _id: "676411590110e98fa92337a1", name: "test1", value: 1 },
+          { _id: "676411590110e98fa92337a2", name: "test2", value: 2 },
+          { _id: "676411590110e98fa92337a3", name: "test3", value: 3 },
+        ], null, 2);
+        assert.include(resultText, expectedText, 'Query result should match');
 
     });
 });
