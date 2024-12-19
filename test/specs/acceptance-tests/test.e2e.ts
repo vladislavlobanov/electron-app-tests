86d95bfbe68a27d7615504637fb8ab560f23eb51
Vaisakh Sasikumar <vaisakh.sasikumar@3tsoftwarelabs.com>
2024-12-19 17:41:29 +0100

    Add 3 acceptance tests

--- test/specs/acceptance-tests/test.e2e.ts
+++ test/specs/acceptance-tests/test.e2e.ts
@@ -18,4 +18,34 @@ describe('MongoDB Query Execution Test', () => {
         assert.include(resultText, 'test2', 'Query result should contain field');
         assert.include(resultText, 'test3', 'Query result should contain field');
     });
+
+    it('should execute a simple unsuccessful query and display error', async () => {
+        const queryInput = await browser.$('.card-body textarea');
+        await queryInput.setValue('{"name":"test4}');
+
+        const runButton = await browser.$('.btn.btn-primary.btn-block');
+        await runButton.click();
+
+        const resultElement = await browser.$('.card-body pre');
+        const resultText = await resultElement.getText();
+
+        assert.include(resultText, 'Invalid query or server error.', 'Query result should not contain "Invalid Query"');
+    });
+});
+
+describe('Advanced View Toggle Test', () => {
+    it('should toggle advanced view and toggle query history', async () => {
+        const advancedViewToggle = await $('.form-check-input');
+        await advancedViewToggle.click();
+
+        const queryHistorySection = await $('.app-history div');
+        await queryHistorySection.waitForDisplayed();
+
+        // Assert that the "Query History" section is visible
+        await expect(queryHistorySection).toBeDisplayed();
+        await advancedViewToggle.click();
+        await expect(queryHistorySection).not.toBeDisplayed();
+
+    });
+
 });
