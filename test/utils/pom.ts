da03980eaf95e5aa57c1197a447b8f289541a02c
andrei-kim-3tsoftwarelabs <andrei.kim@3tsoftwarelabs.com>
2025-01-16 12:28:23 +0100

    udapte smoke tests

--- test/utils/pom.ts
+++ test/utils/pom.ts
@@ -1,12 +1,21 @@
 import { browser } from "@wdio/globals";
 
 export class POM {
+  public async checkApplicationIsReady() {
+    const status = await browser.status();
+    return status.ready;
+  }
+
   public async getTitle() {
     return browser.getTitle();
   }
 
   public getQueryInput() {
     return browser.$("textarea");
+  }
+
+  public async getQueryInputText() {
+    return browser.$("textarea").getText();
   }
 
   public getRunButton() {
