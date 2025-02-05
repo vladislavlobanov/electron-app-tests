7b9becef01e3ed9de36ae2a72f156dfa4974e1e0
Ivan Martynov <ivan.martynov@3tsoftwarelabs.com>
2025-02-05 20:51:36 +0100

    refactoring # 3

--- test/specs/acceptance-tests/utils/dbutils.ts
+++ test/specs/acceptance-tests/utils/dbutils.ts
@@ -1,4 +1,7 @@
-import { join } from "path";
+import { join, dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export async function cleanQueriesTable(): Promise<void> {
   let sqlite3Module;
